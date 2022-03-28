bbd5ffd95efac8fa6aeb2287d8eb33c8fa6b0175
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-28 15:49:16 +0200

    Fixed Movement

--- water-game/Assets/Code/Movement.cs
+++ water-game/Assets/Code/Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private KeyCode jumpKey1;
+    [SerializeField] private KeyCode jumpKey2;
     [SerializeField] private KeyCode moveLeftKey;
     [SerializeField] private KeyCode moveRightKey;
 
@@ -14,7 +15,9 @@ public class Movement : MonoBehaviour
 
     [SerializeField] private float jumpForce;
     [SerializeField] private float moveForce;
+    [SerializeField] private float friction;
 
+    [SerializeField] private float maxSpeed;
 
 
     private Vector3 startPos;
@@ -27,14 +30,15 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(jumpKey))
+        if (Input.GetKeyDown(jumpKey1) || Input.GetKeyDown(jumpKey2))
         {
             if (Groundcheck())
-                playerRigidBody.AddForce(Vector2.up * jumpForce);
+                Accelerate(Vector2.up * jumpForce);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
+            Debug.Log("Speed: " + playerRigidBody.velocity.x);
             transform.position = startPos;
             playerRigidBody.velocity = Vector2.zero;
         }
@@ -44,28 +48,28 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
-
         if (Input.GetKey(moveLeftKey))
         {
-            playerRigidBody.AddForce(Vector2.left * moveForce);
+            Accelerate(Vector2.left * moveForce);
         }
 
         if (Input.GetKey(moveRightKey))
         {
-            playerRigidBody.AddForce(Vector2.right * moveForce);
+            Accelerate(Vector2.right * moveForce);
         }
 
-        
+        if (Input.GetKey(moveLeftKey) == false && Input.GetKey(moveRightKey) == false)
+        {
+            if (Groundcheck())
+            {
+                ApplyFriction();
+            }
+        }
     }
-
 
     private bool Groundcheck()
     {
-        Vector2 newSize = player.transform.localScale;
-        newSize.x *= 1f;
-
-        var collision = Physics2D.OverlapBox(player.transform.position + new Vector3(0f, -1f, 0f), newSize, 0f);
+        var collision = Physics2D.OverlapBox(player.transform.position + new Vector3(0f, 0.1f, 0f), player.transform.localScale, 0f);
 
         if (collision == null)
             return false;
@@ -78,4 +82,25 @@ public class Movement : MonoBehaviour
         return false;
     }
 
+    private void ApplyFriction()
+    {
+        playerRigidBody.velocity *= friction;
+    }
+
+    private void Accelerate(Vector2 acceleration)
+    {
+        
+
+        playerRigidBody.velocity += acceleration;
+
+        
+        float xVel = playerRigidBody.velocity.x;
+        float yVel = playerRigidBody.velocity.y;
+
+        xVel = Mathf.Clamp(xVel, -maxSpeed, maxSpeed);
+        yVel = Mathf.Clamp(yVel, -maxSpeed, maxSpeed);
+
+        playerRigidBody.velocity = new Vector2(xVel, yVel);
+        
+    }
 }
