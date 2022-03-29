37839929bfe1dacc20763c32bcc46a8f5a386c68
Jonathan Lindell <te20jonlin@falufri.se>
2022-03-29 10:13:49 +0200

    PickUp

--- water-game/Assets/Code/PowerUp.cs
+++ water-game/Assets/Code/PowerUp.cs
@@ -7,7 +7,7 @@ abstract public class PowerUp : MonoBehaviour
     public PowerUp powerUp;
     public Player player;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         powerUp.gameObject.SetActive(false);
         player.ApplyPowerEffect(powerUp);
