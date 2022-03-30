aed31d86604faf356419649149c700506f81d514
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-30 17:03:49 +0200

    small fix

--- water-game/Assets/Code/Projectile.cs
+++ water-game/Assets/Code/Projectile.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    /*[SerializeField] public float SpeedModifier = 10.0f;
-    public Vector3 direction;*/
-
     [SerializeField] protected Rigidbody2D rigid_body;
 
     [SerializeField] protected float speed;
     [SerializeField] protected float rotationRate;
-
-    //public bool facingRight;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public void SetDirection(bool facingRight)
     {
@@ -30,7 +19,6 @@ public class Projectile : MonoBehaviour
         {
             rigid_body.velocity = Vector2.left * speed;
         }
-        Debug.Log("speed:"+speed);
     }
 
     public void Rotate()
