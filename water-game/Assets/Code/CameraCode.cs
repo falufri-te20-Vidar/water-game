33297f729c6987f757224759e005e42b46f9fe7c
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-30 15:57:02 +0200

    Camera movement

--- water-game/Assets/Code/CameraCode.cs
+++ water-game/Assets/Code/CameraCode.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraCode : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private GameObject camera;
 
     [SerializeField] private float cameraOffset;
     [SerializeField] private float cameraVelocityModifier;
@@ -19,22 +18,45 @@ public class CameraCode : MonoBehaviour
     void FixedUpdate()
     {
         float playerX = player.transform.position.x;
-        float cameraX = camera.transform.position.x;
+        float playerY = player.transform.position.y;
+
+        float cameraX = transform.position.x;
+        float cameraY = transform.position.y;
+
+        float XcameraSpeed = 0f;
+        float YcameraSpeed = 0f;
         
+        // X movement
         if (playerX < cameraX - cameraOffset)
         {
-            MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            //MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            XcameraSpeed = (playerX - cameraX) * cameraVelocityModifier;
         }
-        else if (player.transform.position.x > camera.transform.position.x + cameraOffset)
+        else if (playerX > cameraX + cameraOffset)
         {
-            MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            //MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            XcameraSpeed = (playerX - cameraX) * cameraVelocityModifier;
         }
+
+        // Y movement
+        if (playerY < cameraY - cameraOffset)
+        {
+            //MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            YcameraSpeed = (playerY - cameraY) * cameraVelocityModifier;
+        }
+        else if (playerY > cameraY + cameraOffset)
+        {
+            //MoveCamera((playerX - cameraX) * cameraVelocityModifier);
+            YcameraSpeed = (playerY - cameraY) * cameraVelocityModifier;
+        }
+
+        MoveCamera(XcameraSpeed, YcameraSpeed);
 
     }
 
-    private void MoveCamera(float cameraSpeed)
+    private void MoveCamera(float XcameraSpeed, float YcameraSpeed)
     {
-        Vector3 velocity = new Vector3(cameraSpeed, 0f, 0f);
-        camera.transform.position += velocity;
+        Vector3 velocity = new Vector3(XcameraSpeed, YcameraSpeed, 0f);
+        transform.position += velocity;
     }
 }
