b593ba71008fc5a3db7d20c69d827406b949e93e
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-30 10:47:10 +0200

    Bigg push

--- water-game/Assets/Code/PowerUp.cs
+++ water-game/Assets/Code/PowerUp.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 abstract public class PowerUp : MonoBehaviour
 {
     public PowerUp powerUp;
-    public Player player;
+    public Player playerCode;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        playerCode = player.GetComponent<Player>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             powerUp.gameObject.SetActive(false);
-            player.ApplyPowerEffect(powerUp);
+            playerCode.ApplyPowerEffect(powerUp);
             FindObjectOfType<AudioManager>().Play("PowerUp");
         }
     }
