b0274ed9b8853edce804f53c5c0fbbe380de63ed
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-30 16:53:31 +0200

    Added platform bounds script

--- water-game/Assets/Code/PlatformBounds.cs
+++ water-game/Assets/Code/PlatformBounds.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlatformBounds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private BoxCollider2D groundBox;
 
-    // Update is called once per frame
+    [SerializeField] private float offset;
+
     void Update()
     {
-        
+        float platformX = transform.localScale.x;
+
+        float xSize = platformX - 2 * offset;
+        xSize = xSize / platformX;
+
+        float ySize = groundBox.size.y;
+
+        groundBox.size = new Vector2(xSize, ySize);
     }
 }
