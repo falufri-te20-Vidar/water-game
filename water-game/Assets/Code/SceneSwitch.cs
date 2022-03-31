321c520a67c22ec59eea89dd7bdc9ecf66d9614d
Jonathan Lindell <te20jonlin@falufri.se>
2022-03-31 09:26:11 +0200

    DoorFix

--- water-game/Assets/Code/SceneSwitch.cs
+++ water-game/Assets/Code/SceneSwitch.cs
@@ -7,12 +7,26 @@ public class SceneSwitch : MonoBehaviour
 {
     [SerializeField] private int thisLevel; //Keeps track of which level the player is on
     public Animator animator;
+    private float timer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "Player")
         {
             animator.SetBool("Open", true);
-            SceneManager.LoadScene(thisLevel); //Changes level depending on what level the player is on
+        }
+    }
+
+    private void Update()
+    {
+        if (gameObject.name == "Respawn" && animator.GetBool("Open") == true)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1)
+            {
+                SceneManager.LoadScene(thisLevel); //Changes level depending on what level the player is on
+                animator.SetBool("Open", false);
+                timer = 0f;
+            }
         }
     }
 }
